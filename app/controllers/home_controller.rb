3d431bd7d5e80054fbc5b8d2b0fdecd2437c2095
Matt Hughes <mhughes27@gmail.com>
2016-07-13 15:33:18 -0500

    got new task  to write to the text file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,6 +13,20 @@ MyApp.get "/new" do
 	erb :"new"
 end
 
+
+MyApp.post "/new/process" do
+	@new_task = params[:task]
+	@new_person = params[:person]
+
+	somefile = File.open("tasks.txt", "a")
+	somefile.puts "i|#{@new_person}|#{@new_task}"
+	somefile.close
+    erb :"process"
+    redirect '/'
+end
+
+
+
 MyApp.get "/edit" do
 	erb :"edit"
 end
