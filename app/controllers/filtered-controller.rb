52985ccee2153d42425064d3a090317292747623
Matt Hughes <mhughes27@gmail.com>
2016-07-15 13:45:14 -0500

    Name added to filtered page

--- app/controllers/filtered-controller.rb
+++ app/controllers/filtered-controller.rb
@@ -1,7 +1,7 @@
 MyApp.get "/filtered" do
 	@theseAreNotDone = Task.familyMemberIncompleteTasks("tasks.txt", params[:user])
 	@theseAreDone = Task.familyMemberCompletedTasks("tasks.txt", params[:user])
-
+	@member = params[:user]
 	erb :"filtered"
 end
 
