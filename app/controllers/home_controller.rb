f41d009cff53ddf0cae2fa6844c38dd40a311efe
Matt Hughes <mhughes27@gmail.com>
2016-07-22 09:12:23 -0500

    essage edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,7 @@ end
 MyApp.post "/new/process" do
 
 	@rightEmail = Task.sortByName(params[:person], "Bob", "Mary", "Joe", "Lisa", "michaeljducey@gmail.com", "michaeljducey@gmail.com", "mhughes27@gmail.com", "mhughes27@gmail.com")
-	sendEmail = Task.sendTaskReminder(@rightEmail, params[:person])
+	sendEmail = Task.sendTaskReminder(@rightEmail, params[:person], params[:task])
 	@newtask = Task.createNewTask("tasks.txt", params[:person], params[:task], rand(99999999))
 	redirect '/'
 end
